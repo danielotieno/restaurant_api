a286494b8aea8bbf7e8fd5c6719b78883849c269
Daniel Otieno <danielotieno.ke@gmail.com>
2020-02-13 08:05:00 +0300

    chore: Creates a simple node server

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+// Load env files
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 7000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on PORT ${PORT}`
+  )
+);
