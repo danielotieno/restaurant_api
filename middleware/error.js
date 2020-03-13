fdba6541e848f7627d285fd8a36d89f41c6931cb
Daniel Otieno <danielotieno.ke@gmail.com>
2020-03-13 17:48:50 +0300

    chore:Mongoose custom error handler

--- middleware/error.js
+++ middleware/error.js
@@ -10,9 +10,22 @@ const errorHanlder = (err, req, res, next) => {
   // Log to console for developer
   logger.info(err.stack);
 
+  // Mongoose bad ObjectId
   if (err.name === 'CastError') {
     const message = 'Resource not found';
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose Dublicate Key
+  if (err.code === 11000) {
+    const message = 'Cannot create same resource twice';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode).json({
